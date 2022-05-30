42c8bb60bf265c2caf28d7bd86fbdf5c21305179
Siu <siuc@protonmail.com>
2022-05-30 18:29:44 -0400

    return int if input is just an int

--- calc.js
+++ calc.js
@@ -1,4 +1,10 @@
+const getResult = (exp) => {
+  if (exp[0] !== "(") return parseInt(exp);
+  return "";
+}
+
 const { argv } = process;
 const userInput = argv[2];
 
-console.log(userInput, userInput + 1);
+const result = getResult(userInput)
+console.log(result);
